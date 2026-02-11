78c2441d00475f1d4a23d327f106d7ec9a446103
Njabulo Jb <fanajb65@gmail.com>
2026-02-11 14:06:23 +0200

    Update remove.js

--- plugins/remove.js
+++ plugins/remove.js
@@ -18,8 +18,8 @@ const kick = async (m, gss) => {
 
     const groupMetadata = await gss.groupMetadata(m.from);
     const participants = groupMetadata.participants;
-    const botAdmin = participants.find(p => p.id === botNumber)?.admin === 'admin';
-    const senderAdmin = participants.find(p => p.id === m.sender)?.admin === 'admin';
+    const botAdmin = participants.find(p => p.id === botNumber)?.admin;
+    const senderAdmin = participants.find(p => p.id === m.sender)?.admin;
 
     console.log('Bot admin:', botAdmin);
     console.log('Sender admin:', senderAdmin);
